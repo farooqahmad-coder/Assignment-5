1667dedf93cdd79096ea1ef3f113bafe99904649
Farooq Ahmad <farooqui1600@gmail.com>
2020-06-14 08:32:43 -0700

    Assignment#5 is completed

---
+++ app1.js
@@ -0,0 +1,168 @@
+         // Chapter#1
+
+// Task#1
+
+// var a = "Dear Visitor! Welcome to our tech world";
+// alert(a);
+
+
+// Task#2
+
+// var a = "Error! Please enter a valid password";
+// alert(a);
+
+
+// Task#3
+
+// alert("Welcome to JS land...","<br>","Happy coding");
+
+
+// Task#4
+
+// var a = "Welcome to js land";
+// var b = "Happy coding";
+// alert(a);
+// alert(b);
+
+
+// Task#5
+
+// var a = "Hello i can run js through my web browser's console";
+// alert(a)
+
+
+// Task#6
+
+// Already done this
+
+
+// Task#7
+
+// Check my html file
+
+
+            // Chapter#2
+
+// Task#1
+
+// var username;
+
+
+// Task#2
+
+// var myName = "Farooq Ahmad";
+
+
+// Task#3
+
+// var message = "Hello World";
+// alert(message);
+
+
+// Task#4
+
+// var name="john doe";
+// var age = "15 years old";
+// var skill = "Certified web application developer";
+// alert(name);
+// alert(age);
+// alert(skill);
+
+
+// Task#5
+
+
+
+
+// Task#6
+
+// var email="farooqui1600@gmail.com";
+// alert("My email address is "+email)
+
+
+// Task#7
+
+// var book = "“A smarter way to learn JavaScript”."
+// alert(book)
+
+
+// Task#8
+
+// var i = "Yah! I can write HTML content through java"
+// document.write(i)
+
+
+
+// Task#9
+
+// var i = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬"
+// alert(i)
+
+
+           // Chapter#3
+
+// Task#1
+
+// var age = 25;
+// alert("I am "+age+" years old")
+
+
+// Task#2
+
+// var visit= 10;
+// alert("You have visited this site "+visit+" times")
+
+
+// Task#3
+
+// var birthYear=1995;
+// document.write("My birth year is "+birthYear,"<br>");
+// document.write("Data type of my declared variable is number")
+
+
+// Task#4
+
+// var userName= "john doe";
+// var title="jeans"
+// var quantity=5;
+// document.write(userName+" ordered "+quantity+" "+title+" on xyzclothes.com")
+
+
+              // Chapter#4
+
+// Task#1
+
+// var name="Farooq",age=25,skill="Developer";
+
+
+// Task#2
+
+// Legal variables
+// var name;
+// var name2user;
+// var $name;
+// var nam_e;
+// var namealert;
+// Illegals
+// var user name="farooq";
+// var alert;
+// var 2name='ali';
+// var _name;
+
+
+// Task#3
+
+// document.write("<h1>Rules for naming JS variables</h1>","<br>")
+// document.write("<p>Variable names can only contain,numbers,$ and_.For example:$my_1stVariable</p>",)
+// document.write("<p>Variables must begin with a letter,$ or_.For example:$name_name or name</p>")
+// document.write("<p>Variable names are case sensitive</p>")
+// document.write("<p>Variable names should not be JS keywords</p>")
+
+
+                // Chapter#5
+
+
+
+
+
+
